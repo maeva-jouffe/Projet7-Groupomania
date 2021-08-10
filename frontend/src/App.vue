b6a368a6f2ae3694c51a707c8b3c50e6cb7494a0
maeva-jouffe <maeva.jouffe@hotmail.fr>
2021-08-10 21:59:09 +0200

    suppression de toute la partie frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,10 @@
 <template>
-  <nav>
-    <router-link to="/">Accueil</router-link>
-    <router-link to="/about">a propos</router-link>
-    <router-link :to="{ name: 'Product', params: { name: 'skate' } }"
-      >Produit 1</router-link
-    >
-  </nav>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
   <router-view/>
 </template>
-
-<script>
-export default {
-  name: "App",
-  components: {},
-};
-</script>
 
 <style>
 #app {
@@ -23,48 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 0;
-   padding: 0;
 }
 
-a {
-  text-decoration: none;
-  color: #999;
+#nav {
+  padding: 30px;
 }
-.card {
-  border: 2px solid black;
-  border-radius: 2em;
-  margin: 2.5em;
-  display: flex;
-  justify-content: center;
-}
-.router-link-active {
+
+#nav a {
   font-weight: bold;
+  color: #2c3e50;
 }
-.card__action:hover {
-  cursor: pointer;
-}
-.button {
-  background: #2196f3;
-  color: white;
-  border-radius: 1em;
-  font-weight: 800;
-  font-size: 15px;
-  border: none;
-  width: 90%;
-  padding: 16px;
-  transition: 0.4s background-color;
-}
-.button:hover {
-  cursor: pointer;
-  background: #1976d2;
-}
-.button--disabled {
-  background: #cecece;
-  color: #ececec;
-}
-.button--disabled:hover {
-  cursor: not-allowed;
-  background: #cecece;
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,58 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '@/views/Login.vue';
-import Profile from '@/views/Profile.vue';
-import Product from '@/views/Product.vue';
-import NotFound from '@/views/NotFound.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
-//Tableau de routes
 const routes = [
-    {
-        name: 'Login',
-        path: '/',
-        component: Login,
-        meta: {
-            title: 'Accueil'
-        }
-    }, {
-        name: 'Profile',
-        path: '/auth/:id',
-        component: Profile,
-        props: true,
-        meta: {
-            title: 'Mon profil'
-        }
-    }, {
-        name: 'Product',
-        path: '/product/:name',
-        component: Product,
-        props: true,
-        meta: {
-            title: 'Produits'
-        }
-    },
-    {
-        name: 'Notfound',
-        path: '/:pathMatch(.*)',
-        component: NotFound,
-        meta: {
-            title: '404 Not Found'
-        }
-    },
-];
-// CREATEWEBHISTORY Lors de l'utilisation du mode historique, l'URL aura l'air "normale", par exemple https://example.com/user/id. Magnifique!
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
 
-// Voici cependant un problème: étant donné que notre application est une application côté client d'une seule page, sans configuration de serveur appropriée, les utilisateurs recevront une erreur 404 s'ils accèdent https://example.com/user/iddirectement dans leur navigateur. Maintenant c'est moche.
-
-// Ne vous inquiétez pas: pour résoudre le problème, il vous suffit d'ajouter une simple route de secours fourre-tout à votre serveur. Si l'URL ne correspond à aucun élément statique, elle doit être diffusée sur la même index.htmlpage que celle sur laquelle se trouve votre application.Magnifique, encore une fois!
-//Création du router
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
-//To = vue d'arrivée. La meta title pour pouvoir mettre la description de la page
-router.afterEach((to) => {
-    document.title = to.meta.title;
-});
-
-export default router;
+export default router
